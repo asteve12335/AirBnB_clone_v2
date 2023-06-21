908b8828671158a1fa18bb369d484a2c5c9952b6
@steve <93563603+asteve12335@users.noreply.github.com>
2023-06-21 01:25:26 +0100

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""
+    contains state class to represent a state
+"""
+
+from models.base_model import BaseModel, Base
+from os import environ
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+import models
+
+storage_engine = environ.get("HBNB_TYPE_STORAGE")
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """ State class: represents states of cities"""
+    if (storage_engine == 'db'):
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """cities list
+            """
+            result = []
+            for k, v in models.storage.all(models.city.City).items():
+                if (k.state_id == self.id):
+                    result.append(k)
+            return result
