68277f8180ced3d7940dd5fea6bb41f34d155a37
asteve12335 <asteve12335@gmail.com>
2023-07-07 00:24:20 +0000

    Prepare your web servers

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+# Preparing Web Server for deployment of hbnb_web_static
+
+sudo apt-get update
+sudo apt-get install -y nginx
+
+sudo rm -rf /etc/nginx/sites-available/default
+sudo rm -rf /etc/nginx/sites-enabled/default
+
+printf %s "server {
+
+	listen 80 default_server;
+	listen [::]:80 default_server;
+
+	add_header X-Served-By $HOSTNAME;
+
+	root /var/www/html;
+	index index.html index.htm index.nginx-debian.html;
+
+	server_name _;
+
+}" > /etc/nginx/sites-available/default
+
+sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/
+
+# Create a redirection
+redirect="\n\tlocation /redirect_me {\n\t\treturn 301 https://youtu.be/dQw4w9WgXcQ;\n\t}"
+sudo sed -i "/server_name _;/a\\$redirect" /etc/nginx/sites-available/default
+
+# custom 404 page
+custom_error_page="\n\terror_page 404 /404.html;\n\tlocation = /404.html {\n\t\troot /var/www/html;\n\t\tinternal;\n\t}"
+sudo sed -i "/server_name _;/a\\$custom_error_page" /etc/nginx/sites-available/default
+
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+
+echo "Holberton School" > /data/web_static/releases/test/index.html
+
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+chown -R ubuntu:ubuntu /data/
+
+web_static="\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t\tindex index.html index.htm index.nginx-debian.html;\n\t}"
+sudo sed -i "/server_name _;/a\\$web_static" /etc/nginx/sites-available/default
+
+# Restart Nginx
+sudo service nginx restart
